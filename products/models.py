226db14d8ad3175203261a3cc43c2c25245e0457
Rakeshlalkn <87595421+Rakeshlalkn@users.noreply.github.com>
2023-05-06 12:46:00 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -17,8 +17,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-    categories = models.ManyToManyField(Category, related_name='products')
-    subcategories = models.ManyToManyField(Subcategory, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ManyToManyField(Category, related_name='products')
+    subcategory = models.ManyToManyField(Subcategory, related_name='products')
 
     def __str__(self):
         return self.name
